2730ac6db9ddb5b946985cefc4c2b43e10a4b54c
Revendz <80220147+Revendz@users.noreply.github.com>
2021-04-18 20:53:42 +0200

    Add files via upload

---
+++ lab7/js/controllers.js
@@ -0,0 +1,48 @@
+var portfolioApp = angular.module('portfolioApp',[]);
+
+portfolioApp.controller('GalleryListCtrl', function($scope)
+{
+    $scope.galleries = 
+    [
+        { 'title':'CLA-2A',
+        'when':'dzis',
+        'thumbnailUrl':'img/galeria/cla.jpg'
+        },
+        { 'title':'H-Delay',
+        'when':'dzis',
+        'thumbnailUrl':'img/galeria/del.jpg'
+        },
+        { 'title':'Doubler',
+        'when':'dzis',
+        'thumbnailUrl':'img/galeria/dob.jpg'
+        },
+        { 'title':'Neutron EQ',
+        'when':'dzis',
+        'thumbnailUrl':'img/galeria/eq.jpg'
+        },
+        { 'title':'Valhalla Reverb',
+        'when':'dzis',
+        'thumbnailUrl':'img/galeria/rev.jpg'
+        },
+        { 'title':'SSL EQ',
+        'when':'dzis',
+        'thumbnailUrl':'img/galeria/ssl.png'
+        }
+    ];
+    $scope.galleries.length;
+    $scope.sortList = 
+    [
+        {
+            'label':'Alfabetycznie',
+            'value':'title'
+        },
+        {
+            'label':'Chronologicznie',
+            'value':'when'
+        },
+        {
+            'label':'Od Najnowszych',
+            'value':'-when'
+        },
+    ];
+});
